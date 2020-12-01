910120d7ffaa553b3c2ec3ae1bccd85f22ce93c7
Encrypted <50642352+Encrypted-Thoughts@users.noreply.github.com>
2020-11-30 22:37:50 -0600

    Initial checkin

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# SLCB-BulkSongParameter
-COMPLETE: The Bulk Song Parameter allows for creating custom commands to add batches of songs from playlist to song queue.
+# SLCB Bulk Song Parameter
+
+The Bulk Song Parameter allows for creating custom commands to add batches of songs from playlist to song queue.
+This is achieved by creating csv files populated with the titles of the songs to be added and saved at the BuilkSongParameter\Song Lists under the script installation folder.
+
+## Installing
+
+This script was built for use with Streamlabs Chatbot.
+Follow instructions on how to install custom script packs at:
+https://github.com/StreamlabsSupport/Streamlabs-Chatbot/wiki/Prepare-&-Import-Scripts
+
+## Use
+
+Once installed the below parameter can be inserted into custom commands created in SLCB.
+```
+$addsongs(
+    string  # File Name: The name of the csv file containing song title to be added.
+)
+
+Example Command: !command add !lofi $addsongs(lofi)
+Example Command: !command add !rock $addsongs(rock)
+```
+
+## Author
+
+EncryptedThoughts - [Twitch](https://www.twitch.tv/encryptedthoughts)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ SongExtensions_StreamlabsSystem.py
@@ -0,0 +1,124 @@
+import clr, json, os, codecs, re, System
+clr.AddReference([asbly for asbly in System.AppDomain.CurrentDomain.GetAssemblies() if "AnkhBotR2" in str(asbly)][0])
+import AnkhBotR2
+
+ScriptName = "Bulk Song Add Parameter"
+Description = "Adds a parameter to allow bulk adding of songs from playlist via a command."
+Creator = "EncryptedThoughts"
+Version = "1.0.0"
+Website = "twitch.tv/encryptedthoughts"
+
+#---------------------------
+#   Define Global Variables
+#---------------------------
+SettingsFile = os.path.join(os.path.dirname(__file__), "settings.json")
+ReadMe = os.path.join(os.path.dirname(__file__), "README.txt")
+
+#---------------------------------------
+# Classes
+#---------------------------------------
+class Settings(object):
+    def __init__(self, SettingsFile=None):
+        if SettingsFile and os.path.isfile(SettingsFile):
+            with codecs.open(SettingsFile, encoding="utf-8-sig", mode="r") as f:
+                self.__dict__ = json.load(f, encoding="utf-8")
+        else:
+            self.EnableDebug = True
+
+    def Reload(self, jsondata):
+        self.__dict__ = json.loads(jsondata, encoding="utf-8")
+        return
+
+    def Save(self, SettingsFile):
+        try:
+            with codecs.open(SettingsFile, encoding="utf-8-sig", mode="w+") as f:
+                json.dump(self.__dict__, f, encoding="utf-8")
+            with codecs.open(SettingsFile.replace("json", "js"), encoding="utf-8-sig", mode="w+") as f:
+                f.write("var settings = {0};".format(json.dumps(self.__dict__, encoding='utf-8')))
+        except:
+            Parent.Log(ScriptName, "Failed to save settings to file.")
+        return
+
+#---------------------------------------
+#   [Required] Initialize Data / Load Only
+#---------------------------------------
+def Init():
+    global ScriptSettings
+    ScriptSettings = Settings(SettingsFile)
+    ScriptSettings.Save(SettingsFile)
+
+#---------------------------
+#   [Optional] Reload Settings (Called when a user clicks the Save Settings button in the Chatbot UI)
+#---------------------------
+def ReloadSettings(jsonData):
+    # Execute json reloading here
+    ScriptSettings.__dict__ = json.loads(jsonData)
+    ScriptSettings.Save(SettingsFile)
+    return
+
+#---------------------------
+#   [Required] Execute Data / Process messages
+#---------------------------
+def Execute(data):
+    return
+
+#---------------------------
+#   [Required] Tick method (Gets called during every iteration even when there is no incoming data)
+#---------------------------
+def Tick():
+    return
+
+#---------------------------
+#   [Optional] Parse method (Allows you to create your own custom $parameters) 
+#---------------------------
+def Parse(parseString, userid, username, targetid, targetname, message):
+
+    regex = "\$addsongs\(.*\)"
+    item = re.search(regex, parseString)
+    if item is not None:
+        fileName = item.group().strip()[10:][:-1]
+
+        if ScriptSettings.EnableDebug:
+            Parent.Log(ScriptName, fileName)
+        filePath = os.path.join(os.path.dirname(__file__) + "\Song Lists", fileName + ".csv")
+        if ScriptSettings.EnableDebug:
+            Parent.Log(ScriptName, filePath)
+        if os.path.isfile(filePath):
+            songManager = AnkhBotR2.Managers.GlobalManager.Instance.SongCom
+            playlist = list(songManager.PlayList)
+            songQueue = list(songManager.SongList)
+
+            with open(filePath) as f:
+                content = f.readlines()
+            for data in content:
+                songs = data.split(",")
+
+                for song in songs:
+                    songName = song.strip()
+                    if ScriptSettings.EnableDebug:
+                        Parent.Log(ScriptName, songName)
+                    if not any(s.Title == songName for s in songQueue):
+                        for playlistSong in playlist:
+                            if playlistSong.Title == songName:
+                                songManager.AddToQueue(playlistSong)
+
+        parseString = parseString.replace(item.group(), "")
+        if ScriptSettings.EnableDebug:
+            Parent.Log(ScriptName, parseString)
+
+    return parseString
+
+#---------------------------
+#   [Optional] Unload (Called when a user reloads their scripts or closes the bot / cleanup stuff)
+#---------------------------
+def Unload():
+    return
+
+#---------------------------
+#   [Optional] ScriptToggled (Notifies you when a user disables your script or enables it)
+#---------------------------
+def ScriptToggled(state):
+    return
+
+def openreadme():
+    os.startfile(ReadMe)
